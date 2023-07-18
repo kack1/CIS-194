d9f04f0b966d136c28610906bb1b7f3155779264
Kyle <kackerman0102@gmail.com>
2023-07-17 21:45:31 -0500

    Finished week5 Exercise 4

--- week5/Homework.hs
+++ week5/Homework.hs
@@ -36,3 +36,32 @@ instance Expr Bool where
   lit = (> 0)
   add = (||)
   mul = (&&)
+
+newtype MinMax =
+  MinMax Integer
+  deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax x) (MinMax y) = MinMax (max x y)
+  mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+newtype Mod7 =
+  Mod7 Integer
+  deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit x = Mod7 (x `mod` 7)
+  add (Mod7 x) (Mod7 y) = Mod7 ((x + y) `mod` 7)
+  mul (Mod7 x) (Mod7 y) = Mod7 ((x * y) `mod` 7)
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+
+testBool = testExp :: Maybe Bool
+
+testMM = testExp :: Maybe MinMax
+
+testSat = testExp :: Maybe Mod7
